4f735b4107ac5f9af8ce1f351492aab51af5ccd4
Gregory Paul Rothstein <g.p.rothstein@gmail.com>
2024-08-10 19:47:03 -0400

    Add a function for rest api

--- functions.php
+++ functions.php
@@ -23,3 +23,16 @@ function custom_scripts()
 {
     wp_enqueue_script('custom-script', get_stylesheet_directory_uri() . '/js/custom.js', array(), '1.0', true);
 }
+
+function my_theme_styles_endpoint()
+{
+    $styles = array(
+        'primary_color' => get_theme_mod('primary_color', '#007cba'), // Example theme mod
+        'secondary_color' => get_theme_mod('secondary_color', '#fff'),
+        // Add other relevant styles
+    );
+
+    return rest_ensure_response($styles);
+}
+
+add_action('rest_api_init', 'my_theme_styles_endpoint');
